bf285836bfa384bcb32639a4f7c4acff9ade0c99
Said Fathalla <sm_fathalla@alex-sci.edu.eg>
2018-12-02 16:29:24 +0100

    initial commit

---
+++ display.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<!-- DW6 -->
+<head>
+<!-- Copyright 2005 Macromedia, Inc. All rights reserved. -->
+<title>SER- Scientific events ranking service</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" href="mm_lodging1.css" type="text/css" />
+<style type="text/css">
+<!--
+.style2 {color: #0066FF}
+.style6 {color: #FFFFFF}
+.style7 {font-size: 14px}
+
+
+table.blueTable {
+  border: 1px solid #1C6EA4;
+  background-color: #EEEEEE;
+  width: 90%;
+  text-align: left;
+  border-collapse: collapse;
+}
+table.blueTable td, table.blueTable th {
+  border: 1px solid #AAAAAA;
+  padding: 1px 2px;
+}
+table.blueTable tbody td {
+  font-size: 13px;
+}
+table.blueTable tr:nth-child(even) {
+  background: #D0E4F5;
+}
+table.blueTable thead {
+  background: #1C6EA4;
+  background: -moz-linear-gradient(top, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+  background: -webkit-linear-gradient(top, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+  background: linear-gradient(to bottom, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+  border-bottom: 2px solid #444444;
+}
+table.blueTable thead th {
+  font-size: 15px;
+  font-weight: bold;
+  color: #FFFFFF;
+  border-left: 2px solid #D0E4F5;
+}
+table.blueTable thead th:first-child {
+  border-left: none;
+}
+
+table.blueTable tfoot td {
+  font-size: 14px;
+}
+table.blueTable tfoot .links {
+  text-align: right;
+}
+table.blueTable tfoot .links a{
+  display: inline-block;
+  background: #1C6EA4;
+  color: #FFFFFF;
+  padding: 2px 8px;
+  border-radius: 5px;
+}
+-->
+</style>
+</head>
+
+<body bgcolor="#999966">
+
+<table width="100%" height="100%" border="0" cellspacing="0" cellpadding="0">
+<!--	<tr>
+	<td width="15" nowrap="nowrap"><img src="mm_spacer.gif" alt="" width="15" height="1" border="0" /></td>
+	<td height="60" colspan="3" class="logo" nowrap="nowrap"><br />
+	  The
+       <span data-sheets-value="{'1':2,'2':'Hybrid Event Rank (HER)'}" data-sheets-userformat="{'2':25089,'3':{'1':0},'12':0,'16':12,'17':1}">Hybrid Event Rank (HER)</span></td>
+	<td width="926">&nbsp;</td>
+	<td width="20">&nbsp;</td>
+	</tr>-->
+
+	<tr bgcolor="#ffffff">
+	<td colspan="6"><img src="mm_spacer.gif" alt="" width="1" height="1" border="0" /></td>
+	</tr>
+
+	<tr bgcolor="#000000">
+	<td width="15" nowrap="nowrap">&nbsp;</td>
+	<td height="36" colspan="3" id="navigation" ><a href="javascript:;">NEWS</a>&nbsp;&nbsp;&nbsp;&nbsp; <a href="javascript:;">ABOUT US</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href="javascript:;">SEO ONTOLOGY </a>&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp; <a href="javascript:;">DATASET</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href="javascript:;" class="pageName style6">CONTACT</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+	<td width="926">&nbsp;</td>
+	<td width="20">&nbsp;</td>
+	</tr>
+
+<tr bgcolor="#ffffff">
+	<td colspan="6"><img src="mm_spacer.gif" alt="" width="1" height="1" border="0" /></td>
+	</tr>
+
+	<tr bgcolor="#ffffff">
+	<td height="1084" colspan="2" valign="top" bgcolor="#ffffff"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <div align="center"><img src="Smart-Data-Analytics.png" alt="Home Page Image" width="151" height="90" border="0" />
+	  </div>
+	  <table border="0" cellspacing="0" cellpadding="0" width="230">
+		<tr>
+		<td width="15">&nbsp;</td>
+		<td width="200" class="sidebarText" id="padding"><div align="justify">The&nbsp;<strong>Smart Data Analytics (SDA)</strong>&nbsp;research group<strong>,&nbsp;</strong>led by<strong>&nbsp;<a href="http://sda.cs.uni-bonn.de/people/prof-dr-jens-lehmann/">Prof. Dr. Jens Lehmann</a>,</strong>&nbsp;has members from the&nbsp;<a href="http://www.iai.uni-bonn.de/">Institute for Computer Science</a>&nbsp;at the&nbsp;<a href="http://www.uni-bonn.de/">University of Bonn</a>, the&nbsp;<a href="http://www.iais.fraunhofer.de/">Fraunhofer Institute for Intelligent Analysis and Information Systems (IAIS)</a>&nbsp;and the&nbsp;<a href="http://infai.org/">Institute for Applied Computer Science</a>&nbsp;Leipzig. At its core, the group investigates machine learning techniques (&quot;analytics&quot;) using&nbsp;structured knowledge (&quot;smart data&quot;). The group&nbsp;aims at covering the full spectrum of research including theoretical foundations, algorithms, prototypes and industrial applications.</div></td>
+		<td width="15">&nbsp;</td>
+		</tr>
+	</table>	</td>
+	<td width="50" valign="top"><img src="mm_spacer.gif" alt="" width="50" height="1" border="0" /></td>
+	<td width="100%" valign="top">
+ <table border="0" cellspacing="0" cellpadding="0" width="100%">
+		<tr>
+		<td class="pageName"><p>Welcome to HER </p></td>
+		</tr>
+
+		<tr>
+		<td class="bodyText">
+		<p class="style7">The
+		  <!--td {border: 1px solid #ccc;}br {mso-data-placement:same-cell;}-->
+		  <span data-sheets-value="{'1':2,'2':'Hybrid Event Rank (HER)'}" data-sheets-userformat="{'2':25089,'3':{'1':0},'12':0,'16':12,'17':1}"><strong>Hybrid</strong> Event Rank (HER)</span> service is a service for ranking computer science scientific events by utilizing several criteria, inclusing rankings from other services sucha as&nbsp;<a href="http://www.core.edu.au" class="style2">CORE</a>, <a href="http://www.capes.gov.br/images/stories/download/avaliacao/Comunicado_004_2012_Ciencia_da_Computacao.pdf" class="style2">Qualis</a>, <a href="http://www.conferenceranks.com/data/era2010_conference_list.pdf" class="style2">ERA</a> and <a href="http://valutazione.unibas.it/gii-grin-scie-rating/ratingSearch.jsf;jsessionid=AFC549DDB0966D0FF2EE2854220120DC" class="style2">GII</a> rankings.</p>
+		</td>
+		</tr>
+	</table>
+
+<?php
+
+	  $acr = $_GET['query'];
+	  $field = $_GET['field'];
+    $title = $_GET['title'];
+$servername = "localhost";
+$username = "root";
+$password = "HER@unibonn2018";
+$dbname = "herdb";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$min_length = 2;
+//if(strlen($acr) >= $min_length || ){ // if query length is more or equal minimum length then
+
+        $acr = htmlspecialchars($acr);
+        // changes characters used in html to their equivalents, for example: < to &gt;
+
+        $acr = $conn->real_escape_string($acr);
+        // makes sure nobody uses SQL injection
+echo $title;
+if (strlen($acr)>0) {
+	$sql = "select   * from events  WHERE (`acronym` LIKE '%".$acr."%' ) ";
+}
+elseif (strlen($field)>8 && strlen($title)==0) { // 8= length(select....)
+	$sql = "select   * from events  WHERE (`field` = '".$field."' ) ";
+}
+elseif (strlen($title)>0) {
+	$sql = "select   * from events  WHERE (`title` LIKE '%".$title."%' ) ";
+}
+else {
+    echo "<font color='red' size='3'>Please select a search criteria.</font>";
+    exit();
+}
+
+//$sql = "select   * from events  WHERE (`acronym` LIKE '%".$acr."%' ) ";
+//$sql = "select   * from events  WHERE (`acronym` LIKE '%".$acr."%' OR `field` = '".$field."') ";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+
+echo" <p Class='style7'> Showing results: <font color='red'><strong>" .mysqli_num_rows($result). "</strong></font> events found.</p>";
+
+echo "<table class='blueTable'><tr bgcolor='#dddddd'><th>acronym</th><th width='330'>title</th><th>type</th><th>SER</th><th>field</th><th>H-index</th><th>avg. aaceptance rate</th><th>Usualy on</th><th>since</th><th>publisher</th></tr>";
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "<tr><td> " . $row["acronym"]. "</td><td>" . $row["title"]. "</td>"."<td>" . $row["Type"]. "</td>"."<td>" . $row["SER"]. "</td>"."<td> ".$row["field"]. "</td>"."<td>" . $row["H5"]. "</td>"."<td>  " . $row["AVGAR"]. "</td>"."<td>  " . $row["UMonth"]. "</td>"."<td>  " . $row["Since"]. "</td>"."<td>  " . $row["Publisher"]. "</td>"."</tr>";
+    }
+	echo "</table>";
+} else {
+    echo "<font color='red' size='3'>No results found.</font>";
+}
+//}
+//else{ // if query length is less than minimum
+ //       echo "Minimum length is ".$min_length;
+ //   }
+
+mysqli_close($conn);
+?>
+
+	  <p>
+        <button onclick="location.href='http://localhost/her/index.html'" type="button">
+        Back</button>
+      </p></td>
+	</tr>
+
+	<tr bgcolor="#ffffff">
+	<td colspan="6"><img src="mm_spacer.gif" alt="" width="1" height="1" border="0" /></td>
+	</tr>
+
+	<tr>
+	<td width="15">&nbsp;</td>
+	<td width="215">&nbsp;</td>
+	<td width="50">&nbsp;</td>
+	<td width="440">&nbsp;</td>
+	<td width="926">&nbsp;</td>
+	<td width="20">&nbsp;</td>
+	</tr>
+</table>
+</body>
+</html>
