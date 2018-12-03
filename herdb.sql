d8e3c6a94f12e7c9168a505f7402e11a8f9fe4f4
Said Fathalla <sm_fathalla@alex-sci.edu.eg>
2018-12-03 13:09:26 +0100

    upload db file

---
+++ herdb.sql
@@ -0,0 +1,127 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 03, 2018 at 12:07 PM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `herdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+DROP TABLE IF EXISTS `events`;
+CREATE TABLE IF NOT EXISTS `events` (
+  `acronym` varchar(20) DEFAULT NULL,
+  `title` varchar(150) DEFAULT NULL,
+  `field` varchar(100) DEFAULT NULL,
+  `H5` varchar(10) DEFAULT NULL,
+  `AVGAR` varchar(10) DEFAULT NULL,
+  `UMonth` varchar(30) DEFAULT NULL,
+  `Since` varchar(10) DEFAULT NULL,
+  `Publisher` varchar(20) DEFAULT NULL,
+  `Type` varchar(20) DEFAULT NULL,
+  `SER` varchar(5) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`acronym`, `title`, `field`, `H5`, `AVGAR`, `UMonth`, `Since`, `Publisher`, `Type`, `SER`) VALUES
+('CVPR', 'IEEE Conference on Computer Vision and Pattern Recognition', 'Artificial Intellligence', '158', '0.33', 'Jun', '1985', 'IEEE', 'Conference', 'A+'),
+('NIPS', 'Advances in Neural Information Processing Systems', 'Artificial Intellligence', '101', '0.25', 'Dec', '1987', 'NIPS', 'Conference', 'A+'),
+('ICCV', 'International Conference on Computer Vision', 'Artificial Intellligence', '89', '0.26', 'Oct', '1987', 'IEEE', 'Conference', 'A+'),
+('IJCAI', 'International Joint Conference on Artificial Intelligence', 'Artificial Intellligence', '45', '0.26', 'Aug', '1969', 'AAArtificial Intelll', 'Conference', 'A+'),
+('AAAI', 'Annual National Conference on Artificial Intelligence', 'Artificial Intellligence', '56', '0.26', 'Jul', '1980', 'AAArtificial Intelll', 'Conference', 'A+'),
+('ECCV', 'European Conference on Computer Vision', 'Artificial Intellligence', '98', '0.31', 'May', '1990', 'Springer', 'Conference', 'A+'),
+('ACCV', 'Asian Conference on Computer Vision', 'Artificial Intellligence', '35', '0.28', 'Jan', '1995', 'Springer', 'Conference', 'B+'),
+('AAMAS', 'International Conference on Autonomous Agents and Multiagent Systems', 'Artificial Intellligence', '40', '0.23', 'May', '2002', 'Springer', 'Conference', 'A+'),
+('UIST', 'ACM Symposium on USer Interface Software and Technology', 'Artificial Intellligence', '44', '0.21', 'Oct', '1988', 'IEEE', 'Symposium', 'A+'),
+('FOGA', 'ACM/SIGEVO Conference on Foundations of Genetic Algorithms', 'Artificial Intellligence', '9', '0.59', 'Jan', '1990', 'ACM', 'Workshop', 'A'),
+('ECAI', 'European Conference on Artificial Intelligence', 'Artificial Intellligence', '29', '0.27', 'Aug', '1982', 'IOS', 'Conference', 'A+'),
+('AISTAT', 'International Conference on Artificial Intelligence and Statistics', 'Artificial Intellligence', '31', '0.35', 'Jan', '1985', 'PMLR', 'Workshop', 'A'),
+('UAI', 'The Conference on UncertArtificial Intellligencenty in Artificial Intelligence', 'Artificial Intellligence', '32', '0.33', 'Jul', '1985', 'IEEE', 'Conference', 'A+'),
+('ICONIP', 'International Conference on Neural Information Processing', 'Artificial Intellligence', '17', '0.54', 'Jan', '1997', 'Springer', 'Conference', 'A'),
+('EUROGP', 'European Conference on Genetic Programming,', 'Artificial Intellligence', '14', '0.36', 'Apr', '1998', 'IOS', 'Conference', 'A'),
+('KR', 'International Conference on Principles of Knowledge Representation and Reasoning', 'Artificial Intellligence', '26', '0.28', 'Apr', '1989', 'AAArtificial Intelll', 'Conference', 'A+'),
+('ISCA', 'International Symposium on Computer Architecture', 'Computer systems organization', '54', '0.18', 'Jun', '1973', 'IEEE', 'Symposium', 'A+'),
+('HPCA', 'High-Performance Computer Architecture', 'Computer systems organization', '46', '0.2', 'Feb', '1995', 'ACM', 'Conference', 'A+'),
+('FOCS', 'Annual Symposium on Foundations of Computer Science', 'Computer systems organization', '45', '0.28', 'Oct', '1989', 'IEEE', 'Symposium', 'A+'),
+('PERCOM', 'International Conference on Pervasive Computing and Communications', 'Computer systems organization', '31', '0.15', 'Mar', '2003', 'IEEE', 'Conference', 'A+'),
+('DSN', 'International Conference on Dependable Systems and Networks', 'Computer systems organization', '32', '0.23', 'Jun', '2000', 'IEEE', 'Conference', 'A+'),
+('SBAC-PAD', 'International Symposium on Computer Architecture and High Performance Computing', 'Computer systems organization', '15', '2.13', 'Oct', '1987', 'IEEE', 'Symposium', 'A'),
+('CSCW', 'ACM Conference on Computer-Supported Cooperative Work and Social Computing', 'Computer systems organization', '56', '0.26', 'Nov', '1986', 'ACM', 'Conference', 'A+'),
+('EuroSys', 'European Conference on Computer Systems', 'Computer systems organization', '41', '0.22', 'Apr', '2006', 'ACM', 'Conference', 'A+'),
+('PODC', 'Symposium on Principles of Distributed Computing', 'Computer systems organization', '25', '0.3', 'Aug', '1982', 'ACM', 'Symposium', 'A+'),
+('ISMAR', 'IEEE / ACM International Symposium on Mixed and Augmented Reality', 'Human Centered Computing', '26', '0.24', 'Oct', '1999', 'IEEE', 'Symposium', 'A+'),
+('VR', 'IEEE Conference on Virtual Reality and 3D User Interfaces', 'Human Centered Computing', '17', '0.26', 'Mar', '1993', 'IEEE', 'Conference', 'A'),
+('CHI', 'Conference on Human Factors in Computing Systems', 'Human Centered Computing', '85', '0.24', 'Apr', '1982', 'ACM', 'Conference', 'A+'),
+('MOBICOM', 'Annual International Conference on Mobile Computing and Networking', 'Human Centered Computing', '45', '0.16', 'sep', '1995', 'ACM', 'Conference', 'A+'),
+('Infovis', 'IEEE Symposium on Information Visualization', 'Human Centered Computing', '14', '0.31', 'Oct', '1990', 'IEEE', 'Conference', 'A+'),
+('SIGGRAPH', 'Special Interest Group on Computer Graphics and Interactive Techniques Conference', 'Human Centered Computing', '22', '0.23', 'Jul', '1974', 'ACM', 'Conference', 'A+'),
+('VLDB', 'International Conference on Very Large Databases', 'IS', '73', '0.18', 'Aug', '1985', 'VLDB', 'Conference', 'A+'),
+('RecSys', 'Conference on Recommender Systems', 'IS', '34', '0.26', 'Oct', '2007', 'ACM', 'Conference', 'A'),
+('EDBT', 'International Conference on Extending Database Technology', 'IS', '32', '0.2', 'Mar', '1988', 'OP', 'Conference', 'A+'),
+('PKDD', 'Principles and Practice of Knowledge Discovery in Databases', 'IS', '31', '0.25', 'Sep', '1997', 'ACM', 'Conference', 'A+'),
+('PODS', 'ACM SIGMOD-SIGACT-SIGArtificial Intellligence Symposium on Principles of Database Systems', 'IS', '26', '0.24', 'Jun', '1982', 'ACM', 'Symposium', 'A+'),
+('SIGIR', 'International ACM SIGIR Conference on Research and Development in Information Retrieval', 'IS', '50', '0.3', 'Aug', '1982', 'ACM', 'Conference', 'A+'),
+('SIGMOD', 'International Conference on Management of Data', 'IS', '59', '0.17', 'Jun', '1979', 'ACM', 'Conference', 'A+'),
+('ICSE', 'International Conference on Software Engineering', 'Software Engineering', '68', '0.17', 'May', '1975', 'ACM', 'Conference', 'A+'),
+('PLDI', 'Conference on Programming Language Design and Implementation', 'Software Engineering', '50', '0.21', 'Jun', '1979', 'ACM', 'Conference', 'A+'),
+('ASPLOS', 'Architectural Support for Programming Languages and Operating Systems', 'Software Engineering', '50', '0.22', 'Mar', '1982', 'ACM', 'Conference', 'A+'),
+('ICDE', 'International Conference on Data Engineering', 'Software Engineering', '51', '0.2', 'Feb', '1984', 'ACM', 'Conference', 'A+'),
+('POPL', 'ACM SIGPLAN Symposium on Principles of Programming Languages', 'Software Engineering', '46', '0.2', 'Jan', '1973', 'ACM', 'Symposium', 'A+'),
+('OOPSLA', 'Conference on Object-Oriented Programming Systems, Languages, and Applications', 'Software Engineering', '37', '0.22', 'Oct', '1986', 'ACM', 'Conference', 'A+'),
+('OSDI', 'USENIX Symposium on Operating Systems Design and Implementation', 'Software Engineering', '39', '0.16', 'Oct', '1994', 'USENIX', 'Symposium', 'A+'),
+('FSE', 'ACM Joint European Software Engineering Conference and Symposium on the Foundations of Software Engineering', 'Software Engineering', '43', '0.18', 'sep', '1993', 'Springer', 'Symposium', 'A+'),
+('ASE', 'IEEE/ACM International Conference on Automated Software Engineering', 'Software Engineering', '31', '0.18', 'Sep', '1991', 'IEEE', 'Conference', 'A'),
+('ICFP', 'International Conference on Functional Programming', 'Software Engineering', '27', '0.31', 'sep', '1996', 'ACM', 'Conference', 'A+'),
+('ECOOP', 'European Conference on Object-Oriented Programming', 'Software Engineering', '24', '0.2', 'jul', '1987', 'ACM', 'Conference', 'A+'),
+('FASE', 'Fundamental Approaches to Software Engineering', 'Software Engineering', '23', '0.23', 'mar', '1998', 'ACM', 'Conference', 'A'),
+('CCS', 'Conference on Computer and Communications Security', 'Compuyter Security', '72', '0.22', 'Oct', '1993', 'ACM', 'Conference', 'A+'),
+('SP', 'IEEE Symposium on Security and Privacy', 'Compuyter Security', '68', '0.28', 'May', '1980', 'IEEE', 'Symposium', 'A+'),
+('USENIX', 'Usenix Security Symposium', 'Compuyter Security', '61', '0.19', 'Aug', '1990', 'USENIX', 'Conference', 'A+'),
+('NDSS', 'Annual Network and Distributed System Security Symposium', 'Compuyter Security', '56', '0.2', 'Feb', '1993', 'NDSS', 'Symposium', 'A+'),
+('EuroCrypt', 'International Conference on the Theory and Applications of Cryptographic Techniques', 'Compuyter Security', '33', '0.24', 'May', '1982', 'Springer', 'Conference', 'A+'),
+('Ubicomp', 'ACM International Joint Conference on pervasive and Ubiquitous Computing', 'Compuyter Security', '50', '0.17', 'Sep', '1999', 'ACM', 'Conference', 'A+'),
+('ACSAC', 'Annual Computer Security Applications Conference', 'Compuyter Security', '29', '0.24', 'Dec', '1985', 'ACM', 'Conference', 'A+'),
+('CSF', 'IEEE Computer Security Foundations Symposium', 'Compuyter Security', '23', '0.3', 'Jun', '1988', 'IEEE', 'Symposium', 'A+'),
+('ESORICS', 'European Symposium on Research in Computer Security', 'Compuyter Security', '28', '0.22', 'sep', '1992', 'Springer', 'Symposium', 'A+'),
+('DCC', 'Data Compression Conference', 'Theory of Computation', '17', '', 'Mar', '1991', 'IEEE', 'Conference', 'A+'),
+('IJCAR', 'International Joint Conference on Automated Reasoning', 'Theory of Computation', '45', '0.41', 'Jul', '2001', 'ACM', 'Conference', 'A+'),
+('COLT', 'Conference on Learning Theory', 'Theory of Computation', '33', '0.49', 'Jun', '1988', 'PMLR', 'Conference', 'A+'),
+('STOC', 'Annual ACM SIGACT Symposium on Theory of Computing', 'Theory of Computation', '56', '0.35', 'May', '1969', 'ACM', 'Symposium', 'A+'),
+('SPAA', 'ACM Symposium on Parallelism in Algorithms and Architectures', 'Theory of Computation', '21', '0.32', 'Jun', '1989', 'ACM', 'Symposium', 'A+'),
+('CCC', 'Computational Complexity Conference', 'Theory of Computation', '19', '0.33', 'Jun', '1986', 'ACM', 'Conference', 'A+'),
+('ISSAC', 'International Symposium on Symbolic and AlgebrArtificial Intellligencec Computation', 'Theory of Computation', '18', '0.47', 'Jul', '1988', 'ACM', 'Conference', 'A+'),
+('TheWeb', 'International Conference on World Wide Web', 'Web Technologies', '77', '0.17', 'May', '1995', 'TheWeb', 'Conference', 'A+'),
+('ICWS', 'IEEE International Conference on Web Services', 'Web Technologies', '40', '0.21', 'Jun', '1995', 'IEEE', 'Conference', 'A+'),
+('WSDM', 'ACM International Conference on Web Search and Data Mining', 'Web Technologies', '54', '0.18', 'Feb', '2004', 'ACM', 'Conference', 'A+'),
+('ESWC', 'European Semantic Web Conference', 'Web Technologies', '40', '0.25', 'May', '2004', 'Springer', 'Conference', 'A+'),
+('ISWC', 'International Semantic Web Conference', 'Web Technologies', '26', '0.24', 'Oct', '1997', 'Springer', 'Conference', 'A+'),
+('ICWE', 'International Conference on Web Engineering', 'Web Technologies', '18', '0.24', 'Jul', '2001', 'IEEE', 'Conference', 'B+'),
+('ICSC', 'IEEE International Conference on Semantic Computing', 'Web Technologies', '15', '0.28', 'Sep', '2007', 'IEEE', 'Conference', 'A');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
